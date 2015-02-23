99334b16fc4ce90ee21663a8c60d53103255c87f
Aleks Kissinger <aleks0@gmail.com>
2015-02-23 10:11:52 +0000

    added quantojs example

---
+++ quantojs/js/texconstants.js
@@ -0,0 +1,39 @@
+
+function texConstants(s) {
+  var t = s;
+  t = t.replace("\\alpha", "\u03b1");
+  t = t.replace("\\beta", "\u03b2");
+  t = t.replace("\\gamma", "\u03b3");
+  t = t.replace("\\delta", "\u03b4");
+  t = t.replace("\\epsilon", "\u03b5");
+  t = t.replace("\\zeta", "\u03b6");
+  t = t.replace("\\eta", "\u03b7");
+  t = t.replace("\\theta", "\u03b8");
+  t = t.replace("\\iota", "\u03b9");
+  t = t.replace("\\kappa", "\u03ba");
+  t = t.replace("\\lambda", "\u03bb");
+  t = t.replace("\\mu", "\u03bc");
+  t = t.replace("\\nu", "\u03bd");
+  t = t.replace("\\xi", "\u03be");
+  t = t.replace("\\pi", "\u03c0");
+  t = t.replace("\\rho", "\u03c1");
+  t = t.replace("\\sigma", "\u03c3");
+  t = t.replace("\\tau", "\u03c4");
+  t = t.replace("\\upsilon", "\u03c5");
+  t = t.replace("\\phi", "\u03c6");
+  t = t.replace("\\chi", "\u03c7");
+  t = t.replace("\\psi", "\u03c8");
+  t = t.replace("\\omega", "\u03c9");
+  t = t.replace("\\Gamma", "\u0393");
+  t = t.replace("\\Delta", "\u0394");
+  t = t.replace("\\Theta", "\u0398");
+  t = t.replace("\\Lambda", "\u039b");
+  t = t.replace("\\Xi", "\u039e");
+  t = t.replace("\\Pi", "\u03a0");
+  t = t.replace("\\Sigma", "\u03a3");
+  t = t.replace("\\Upsilon", "\u03a5");
+  t = t.replace("\\Phi", "\u03a6");
+  t = t.replace("\\Psi", "\u03a8");
+  t = t.replace("\\Omega", "\u03a9");
+  return t;
+}
